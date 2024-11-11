6bf138814b0be766b4fc0cdfd656b7b80ff02ba0
Yusuf Erdoğan <136724876+yusuferdogan38@users.noreply.github.com>
2024-11-11 16:45:32 +0300

    Jwt first commit

---
+++ Core/Utilities/Helpers/FileHelper/FileHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Helpers.FikeHelper
+{
+    public static class FileType
+    {
+        public const string images = "images";
+        public const string root = "wwwroot/";
+    }
+}
---
+++ Core/Utilities/Helpers/GuidHelper/GuidHelper_.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Helpers.GuidHelper
+{
+    public class GuidHelper_
+    {
+        public static string Create()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
